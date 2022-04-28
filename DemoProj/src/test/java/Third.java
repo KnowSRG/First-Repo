c3678244b2dfdb61fcba603f84b12bfac711d129
knowledz202z <knowledz202z@gmail.com>
2022-04-28 10:34:46 -0500

    test

--- DemoProj/src/test/java/Third.java
+++ DemoProj/src/test/java/Third.java
@@ -1,9 +1,28 @@
+import java.awt.AWTException;
+import java.awt.Robot;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
 
 public class Third {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Third");
-	}
+	public static WebDriver driver;
+	@Test()
+	public void Launch() throws AWTException {
+			System.setProperty("webdriver.chrome.driver","C:\\eclipse\\inetDemoV1\\Drivers\\chromedriver.exe");
+			driver=new ChromeDriver();
+			driver.get("https://jqueryui.com/droppable/");
+			
+			/*driver.switchTo().frame(0);
+		WebElement src=driver.findElement(By.xpath("//div[@id='draggable']"));
+		WebElement target=driver.findElement(By.xpath("//div[@id='droppable']"));
+		Actions act= new Actions(driver);
+			act.dragAndDrop(src, target).build().perform();
+			driver.findElement(By.cssSelector("[alt=Support the JS Foundation]" )).click();_*/
+	}	
 
 }
